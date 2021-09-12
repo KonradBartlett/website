6c93e3d222cce287e0741d1eda1c148678a3ec1e
Konrad Bartlett <KonradBartlett@outlook.com>
2021-09-12 01:31:56 -0700

    updated resume
    detail elements contain more in depth detail
    updating styling to look nicer

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -6,128 +6,80 @@ export function Resume() {
     document.title = "Resume - Konrad Bartlett"
     
     return (
-        <div className="resume">
-            <ScavengerButton colour="red" index="2" mode="avoid"/>
-            <div className="card">
-                <h2>Konrad Bartlett</h2>
-                <hr/>
-
-                <h3>Education</h3>
-                <h4>B.Sc.H | Trent University | 2019 </h4>
-                <ul>
-                    <li>Major: Computation and Information Systems</li>
-                    <li>Specialization: Data Analytics</li>
-                </ul>
-
-                <h3>Certificates</h3>
-                <ul>
-                    <li><h4>Google IT Support Professional Certificate | Google | 2021</h4></li>
-                </ul>
-
-                <h3>Work Experience</h3>
-                <h4>Full Stack Developer | Floatplane Inc. | 2021 - Present</h4>
-                <ul>
-                    <li>Shopify, React, GraphQL</li>
-                    <li>Develop and maintain the merchandise store for a large media company</li>
-                    <li></li>
-                </ul>
-
-                <h4>Full Stack Developer | Gyroscopic Technologies | 2020 - 2021</h4>
-                <ul>
-                    <li>.NET Core, React, React Native, Postres, AWS</li>
-                    <li>Create a web application for creating customizable surveys used to analyze a company’s eligibility for remote work</li>
-                    <li>Work with a team of developers to develop applications using React and related technologies</li>
-                    <li>Communicate with clients to do requirement specification on projects and ensure the application fit requirement needs</li>
-                </ul>
-
-                <h4>Full Stack Developer | TS Manufacturing | 2019 - 2020</h4>
-                <ul>
-                    <li>ASP.NET, Angular, PHP, Micorosft SQL, Microsoft NAV, AutoCAD</li>
-                    <li>Develop in house web applications used for project management and data management</li>
-                    <li>Work with a team of developers using the Agile management structure to develop Autodesk - AutoCAD / Inventor plugins</li>
-                    <li>Interact with key stakeholders to do requirement specification on projects and develop software that was right for them</li>
-                </ul>
-
-                <h4>IT Desk Student Analyst | Trent IT | 2018 - 2019</h4>
-                <ul>
-                    <li>Interact with incoming students and faculty at a service desk and help them resolve their problem</li>
-                    <li>Maintain electronic devices around campus, fixing IT related issues for a variety of cases</li>
-                    <li>Main Switch board for Trent University, listen to caller’s requests and transferring them to appropriate department</li>
-                </ul>
-
-                <h4>TA and Student Grader | Trent University | 2018 - 2019</h4>
-                <ul>
-                    <li>Undergraduate teaching assistant for COIS 2300: Computer Hardware Organization and Design and COIS 4400: Data Mining</li>
-                    <li>Create rubrics and answer keys for marking purposes, as well as grading students’ assignments, midterms, and exams</li>
-                    <li>Engaging and interacting with students inside and outside of classroom to assist them in their learning goals</li>
-                </ul>
-
-                <h4>Data Analyst Internship | Lowfoot | 2018</h4>
-                <ul>
-                    <li>Hadoop, SQL, Java, Python</li>
-                    <li>Create a data warehouse to store device data about IoT devices and energy consumption</li>
-                    <li>Create interface for data warehouse and use data collected to train neural networks to predict energy spikes</li>
-                    <li>Work in close communication with internship partner and employers to ensure that an optimal program is produced</li>
-                </ul>
-            </div>
-            <a className="resume_button" href="./documents/KonradBartlett_Resume.pdf" download="KonradBartlett_Resume.pdf">Download Resume</a>
-        </div>
-    );
-}
-
-
-export function Resume2() {
-    document.title = "Resume - Konrad Bartlett"
-    
-    return (
-        <div className="resume">
+        <div className="page resume">
             <ScavengerButton colour="red" index="2" mode="avoid"/>
             <div className="card cv">
 
 
-                <h1>Konrad Bartlett</h1>
+                <section className="header">
+                    <h1>Konrad Bartlett</h1>
+                    <a className="resume_button dark_hover" href="./documents/KonradBartlett_Resume.pdf" without rel="noopener noreferrer" target="_blank">Download Resume</a>
+                </section>
+                <hr/>
                 <section className="top">
-                    <div>
+                    {/* <div>
                         <div>
                             <h4>Specialty 1</h4>
                             <p>Text</p>
                         </div>
-                    </div>
+                    </div> */}
                     <div className="bars">
                         <details>
                             <summary>
                                 React
                                 <ProgressBar completed={95} isLabelVisible={false} bgColor="#20b573"/>
                             </summary>
-                            <p>test</p>
+                            <p className="detail_blurb">
+                                React is my preferred Javascript framework. 
+                                I use React for all of my personal projects if possible as well as at work. 
+                                I used to be a huge Angular fan boy however I got a work position that required that I work in React, and I haven't looked back since. 
+                                I have several years of experience using React professionally, as well for personal projects and I consider myself to be quite skilled in developing with it.
+                            </p>
                         </details>
                         <details>
                             <summary>
                                 CSS + SASS
                                 <ProgressBar completed={83} isLabelVisible={false} bgColor="#7fc356"/>
                             </summary>
-                            <p>test</p>
+                            <p>
+                                Being a Professional front end developer I spend a large portion of my time staring at CSS, modifying and tweaking properties until I get the element looking just right. 
+                                I prefer to use SCSS files over plan CSS for the extra flexibility and efficiency that it offers when creating style sheets. 
+                                I love finding tutorials online todo novel things like animations, or really standard things as efficiently as possible. 
+                            </p>
                         </details>
                         <details>
                             <summary>
                                 .NET
                                 <ProgressBar completed={67} isLabelVisible={false} bgColor="#f4c70c"/>
                             </summary>
-                            <p>test</p>
+                            <p>
+                                .NET is the first framework that I really fell in love with and spent a large amount of time with as C# is what was taught at school. 
+                                Before I discovered the magic of Javascript and web development, I would always write my personal projects in C# prefering it's strongly typed and object oriented nature over other coding paradigms. 
+                                I have several years experience as a full stack developer where I used some variation of .NET to develop APIs whether it be ASP.NET or .NET Core, I find the LINQ system extremely useful when creating controllers and data models.    
+                            </p>
                         </details>
                         <details>
                             <summary>
                                 Angular
-                        <ProgressBar completed={67} isLabelVisible={false} bgColor="#f4c70c"/>
+                                <ProgressBar completed={67} isLabelVisible={false} bgColor="#f4c70c"/>
                             </summary>
-                            <p>test</p>
+                            <p>
+                                Angular is the first Javascript framework that I used to create professional web applications. 
+                                When I got my first position as a web developer the company that I worked for required the use of Angular as the framework for the front end of applications. 
+                                It was the first time I had really dug into and used Javascript to it's fullest potential and I really fell in love and adopted Angular as my favourite framework for a while. 
+                                I used to ask for Angular project textbooks for Christmas that would provide hundreds of pages of content outlining and walking through different web applications that could be made using Angular and I learned a great deal from following along. 
+                            </p>
                         </details>
                         <details>
                             <summary>
                                 Shopify
-                        <ProgressBar completed={50} isLabelVisible={false} bgColor="#f47938"/>
+                                <ProgressBar completed={50} isLabelVisible={false} bgColor="#f47938"/>
                             </summary>
-                            <p>test</p>
+                            <p>
+                                I have created Features, Apps, and Themes for Shopify professionally for a media company for a while now. 
+                                Shopify development is something that I am still getting the hang of, however the more I learn the more I am finding Shopify to be a robust and enjoyable platform to develop for. 
+                                I write my Features using Preact, my Apps using Apollo GraphQL and NodeJS, and my Themes using Liquid however I am excited for Shopify to finally release their 'Hydrogen' theme which promises to be a theme that entirely uses React and TypeScript.
+                            </p>
                         </details>
                     </div>
                 </section>
@@ -137,17 +89,18 @@ export function Resume2() {
                     <div>
                         <div>
                             <h4>B.Sc.H Computation and Information Systems<br/>Trent University | 2019</h4>
-                            <p>Degree: Honors Bachelor of Science<br/>
-                            Graduation Date: 2019<br/>
-                            Major: Computation and Information Systems<br/>
-                            Specialization: Data Science</p>
+                            <p>
+                            <b>Degree:</b> Honors Bachelor of Science<br/>
+                            <b>Graduation Date:</b> 2019<br/>
+                            <b>Major:</b> Computation and Information Systems<br/>
+                            <b>Specialization:</b> Data Science</p>
                         </div>
                     </div>
 
                     <h2>Certificates</h2>
                     <div>
                         <div>
-                            <h4>Google IT Support Professional Certificate | Google | 2021</h4>
+                            <h4>Google IT Support Professional Certificate<br/>Google | 2021</h4>
                         </div>
                     </div>
                 </section>
@@ -191,7 +144,6 @@ export function Resume2() {
                 </section>
 
             </div>
-            <a className="resume_button" href="./documents/KonradBartlett_Resume.pdf" without rel="noopener noreferrer" target="_blank">Download Resume</a>
         </div>
     );
 }
