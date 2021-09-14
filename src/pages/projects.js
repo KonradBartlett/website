3a413a1ef1c935ab2a967ba11b41542d7dccea26
Konrad Bartlett <Konrad.Bartlett@outlook.com>
2021-09-13 18:54:54 -0700

    fixed react scripts error

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,6 @@
 import { ScavengerButton } from "../components/scavenger";
 import './projects.scss';
+import { Reviews } from "./projects/reviews";
 
 export function Projects() {
     document.title = "Projects - Konrad Bartlett"
@@ -7,8 +8,13 @@ export function Projects() {
         <div className="page">
             <ScavengerButton index="3" colour="rainbow" mode="fade" />
             <div className="card projects">
-                <h2>Projects</h2>
+                <h1>Projects</h1>
                 Coming soon
+                {/* <details>
+                    <summary><h3>Game Reviews</h3></summary>
+                    I like to review videogames!
+                    <Reviews/>
+                </details> */}
             </div>
         </div>
         
--- src/pages/projects/reviews.js
+++ src/pages/projects/reviews.js
@@ -1,9 +1,16 @@
+
+import { useEffect, useState } from 'react';
 import './reviews.scss';
 
 export const Reviews = () => {
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+      }, []);
+    
     return (
         <div className="reviews">
-
+            
         </div>
     );
 }
