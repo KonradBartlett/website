64c84290df390f0b688a924742070d00eb99c39a
Konrad Bartlett <Konrad.Bartlett@outlook.com>
2021-09-14 02:52:40 -0700

    added rgb to header and logo

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,9 +4,9 @@ import { ScavengerButton } from "../components/scavenger";
 export function Home() {
     document.title = "Home - Konrad Bartlett"
     return (
-        <div className="page">
+        <div className="page ">
             <ScavengerButton  index="1" mode="bounce"/>
-            <header>
+            <header className="header">
                 <h1>Konrad Bartlett<br/>Web Developer</h1>
                 <p>React | Angular | Node.js | Shopify | .NET</p>
                 <Links/>
